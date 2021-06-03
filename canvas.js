cb66095ea72897e0f8eb649ae691405819dd1a6b
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2021-06-03 16:52:11 +0700

    popup

--- canvas.js
+++ canvas.js
@@ -1,7 +1,8 @@
 // GET DOM
-const canvas = document.querySelector("#canvas");
-const clear_btn = document.querySelector("#clear");
-const undo_btn = document.querySelector("#undo");
+const canvas = document.querySelector('#canvas');
+const clear_btn = document.querySelector('#clear');
+const undo_btn = document.querySelector('#undo');
+const board_paint = document.querySelector('.board-paint');
 
 // VARIABLES
 let draw_color = 'black';
@@ -15,20 +16,30 @@ let index = -1;
 canvas.width = window.innerWidth;
 canvas.height = 700;
 
-let context = canvas.getContext("2d");
+let context = canvas.getContext('2d');
 context.fillStyle = start_background_color;
-context.fillRect(0,0, canvas.width, canvas.height);
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 // funtions
-function start(e){
+function setBdBoardPaint(imgUrl) {
+    console.log(imgUrl);
+
+    if (imgUrl) {
+        board_paint.style.backgroundImage = `url(${imgUrl})`;
+    }
+}
+function start(e) {
     is_drawing = true;
     context.beginPath();
     context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     e.preventDefault();
 }
-function draw(e){
-    if(is_drawing){
-        context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+function draw(e) {
+    if (is_drawing) {
+        context.lineTo(
+            e.clientX - canvas.offsetLeft,
+            e.clientY - canvas.offsetTop,
+        );
         context.strokeStyle = draw_color;
         context.lineWidth = draw_width;
         context.lineCap = 'round';
@@ -37,46 +48,52 @@ function draw(e){
     }
     e.preventDefault();
 }
-function stop(e){
-    if(is_drawing){
+function stop(e) {
+    if (is_drawing) {
         context.stroke();
         context.closePath();
         is_drawing = false;
     }
     e.preventDefault();
-    if(e.type != 'mouseout'){
-        restore_array.push(context.getImageData(0,0,canvas.width, canvas.height));
+    if (e.type != 'mouseout') {
+        restore_array.push(
+            context.getImageData(0, 0, canvas.width, canvas.height),
+        );
         index += 1;
     }
 }
 function clearCanvas() {
     context.fillStyle = start_background_color;
-    context.clearRect(0,0,canvas.width, canvas.height);
-    context.fillRect(0,0,canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     restore_array = [];
     index = -1;
 }
-function undo(){
-    if(index <= 0){
+function undo() {
+    if (index <= 0) {
         clearCanvas();
     } else {
         index -= 1;
         restore_array.pop();
-        context.putImageData(restore_array[index],0,0);
+        context.putImageData(restore_array[index], 0, 0);
     }
 }
 
+function handleImageOnCanvas(img) {
+    setBdBoardPaint(img);
+    toggle();
+}
 
 // add event to call functions
-canvas.addEventListener('touchstart', start ,false);
-canvas.addEventListener('touchmove', draw ,false);
-canvas.addEventListener('mousedown', start ,false);
-canvas.addEventListener('mousemove', draw ,false);
+canvas.addEventListener('touchstart', start, false);
+canvas.addEventListener('touchmove', draw, false);
+canvas.addEventListener('mousedown', start, false);
+canvas.addEventListener('mousemove', draw, false);
 
-canvas.addEventListener('touchend', stop ,false);
-canvas.addEventListener('mouseup', stop ,false);
-canvas.addEventListener('mouseout', stop ,false);
+canvas.addEventListener('touchend', stop, false);
+canvas.addEventListener('mouseup', stop, false);
+canvas.addEventListener('mouseout', stop, false);
 
 clear_btn.addEventListener('click', clearCanvas);
-undo_btn.addEventListener('click',undo);
+undo_btn.addEventListener('click', undo);
--- patternDrawing.js
+++ patternDrawing.js
@@ -1,1 +1,15 @@
 // Phần này của Long
+const blur = document.getElementById('blur');
+const popup = document.getElementById('popup');
+
+function toggle() {
+    blur.classList.toggle('active');
+    popup.classList.toggle('active');
+}
+
+function onloadPopup() {
+    blur.classList.toggle('active');
+    popup.classList.toggle('active');
+}
+
+onloadPopup();
